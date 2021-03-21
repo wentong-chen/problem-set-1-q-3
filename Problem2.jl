dfb54c05b638fac75ea34e9f63ec9ee7e45511a7
my497 <my497@cornell.edu>
2021-03-21 13:17:54 -0400

    update

--- Problem2.jl
+++ Problem2.jl
@@ -1,5 +1,10 @@
-x = rand(Float64, (20000,20000))
+x = rand(Float64, (2,2))
+exp_column = zeros((2,2))
 function exp_cols(x)
-    i = 1
-    while i <=20000
-        exp_cols
+    i=1
+    while i<=20000
+        cols = exp.(x[:,i])
+        exp_column[:,i] = cols
+        i = i+1
+    end
+end
